0d0e2c5b095ee0d7dbbe6e8faa01b4c4439fc3c9
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2023-11-29 15:20:02 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -215,9 +215,9 @@ with col3:
  st.plotly_chart(fig, use_container_width=True)
 
 with metricas:
- st.metric('Total ovos coletados', dados_ovitrampas_municipio['eggs'].sum())
- st.metric('Ovitrampas inspecionadas', dados_ovitrampas_municipio['ovitrap_id'].count())
- st.metric('Municípios', dados_ovitrampas_municipio['municipality'].count())
+ st.metric('Total ovos coletados', value = dados_ovitrampas_municipio['eggs'].sum())
+ st.metric('Ovitrampas inspecionadas', value = dados_ovitrampas_municipio['ovitrap_id'].count())
+ st.metric('Municípios', value = dados_ovitrampas_municipio['municipality'].count())
 
 css='''
 [data-testid="stMetric"] {
