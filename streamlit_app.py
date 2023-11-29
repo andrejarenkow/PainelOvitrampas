6c41e3bc51ee6e41d4422f65e80d3a44b98eea64
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2023-11-29 15:07:25 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -204,6 +204,7 @@ with col2:
 #tabela com as ovitrampas
 with col3:
  dados_ovitrampas_municipio = pd.pivot_table(dados_grafico, index='ovitrap_id', columns='week_year', values='eggs', aggfunc='sum').fillna('-')
+ dados_ovitrampas_municipio.index.names = ['Nº Ovitrampa']
  st.dataframe(dados_ovitrampas_municipio, height=300, use_container_width=False,)
 
  # Plot!
