f657f25f4b73d7da1ea198f12cadbcb488605d7e
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2023-11-29 10:49:53 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,204 +1,67 @@
 #Importação das bibliotecas 
-import numpy as np
+import requests
 import pandas as pd
-import plotly
-from plotly.graph_objs import Scatter, Layout, Heatmap
-import plotly.graph_objs as go
-import streamlit as st
-import matplotlib
-from bs4 import BeautifulSoup
-from urllib.request import urlopen
-import time
-import plotly.figure_factory as ff
 import plotly.express as px
-from bs4 import BeautifulSoup
-import pandas as pd
-import numpy as np
-from urllib.request import urlopen, Request
-from urllib.parse import urlencode
+import folium
+from streamlit_folium import st_folium
+
  
 # Configurações da página
 st.set_page_config(
-    page_title="Leilão LetraViva",
-    page_icon="	:book:",
+    page_title="Ovitrampas",
+    page_icon="	:bug:",
     layout="wide",
     initial_sidebar_state='collapsed'
 ) 
 col1, col2, col3 = st.columns([2,12,1])
 
 col1.image('https://www.letravivaleiloes.com.br/custom/imagens/logo.png', width=200)
-col2.title('Monitoramento Leilão Letra Viva')
-col2.caption('https://www.letravivaleiloes.com.br/leilao.asp?Num=38762')
+col2.title('Painel de Monitoramento de Aedes aegypti através de Ovitrampas')
+col1.image('https://www.letravivaleiloes.com.br/custom/imagens/logo.png', width=200)
 
-#with st.form('Atualizar dados!'):
-#    st.write('Clique aqui para atualizar')
 
-    # Every form must have a submit button.
-#    submitted = st.form_submit_button("Submit")
-#    if submitted:
-
-#criando as listas que serão os Datasets
-@st.cache_data(ttl=600)
-def load_data(numero):
- precos = []
- descricoes = []
- lances = []
- visitas = []
- links = []
- leilao_catalogo = []
- lote_vendido = []
- imagens = []
- bids = []
  
- dados = pd.DataFrame()
- 
- for catalogo in [numero]:
-   print(catalogo)
-   tamanho = 0
- 
-   for i in range(1,15,1):
-     print(i)
-     url = 'https://www.letravivaleiloes.com.br/catalogo.asp?Num='+catalogo+'&pag=' + str(i)
-     response = urlopen(url)
-     html = response.read()
-     soup = BeautifulSoup(html, 'html.parser')
- 
-     #print(i)
- 
-     if len(soup('p',{'class':'price-bid'}))>0:
-       #print('contém esta página')
-       lista_precos = soup('p',{'class':'price-bid'})
-       for numero in range(len(lista_precos)):
-         if (numero % 2) == 0:
-           preco = lista_precos[numero].get_text()
- 
-           precos.append(preco)
- 
-       lista_descricao = soup.findAll('div', {'class':'twelve columns product-description'})
-       tamanho  = tamanho + len(lista_descricao)
- 
-       for i,j in zip(soup('div',{'class':'lotevendido lote-control'}), range(len(soup('div',{'class':'lotevendido lote-control'})))):
-         if j % 3 == 0:
-           lote_vendido.append(i.get_text().strip())
- 
-       for item in lista_descricao:
-         descricao = item.get_text()
-         descricoes.append(descricao)
- 
-       lista_lances = soup.findAll('strong')
-       for item in lista_lances:
-             lance = item.get_text()
-             lances.append(lance)
- 
- 
-       lista_visitas = soup.findAll('div',{'class':'extra-info-lance'})
-       for item in lista_visitas:
-         visita = item.find('span').get_text()
-         visitas.append(visita)
- 
- 
-       for link in soup.findAll('div', {'class':'product-image zoom_01'}):
-         imagens.append(link.img['src'])
-         links.append('https://www.letravivaleiloes.com.br/'+link.img['value'])
- 
-       for i,j in zip(soup('div',{'class':'product-price-bid'}), range(len(soup('div',{'class':'product-price-bid'})))):
-         if j % 3 == 0:
-           bids.append(i.get_text().strip().split('\r')[0])        
- 
-   print(tamanho)
-   leilao_catalogo.extend([catalogo]*tamanho)
- 
- 
-     #print(len(precos))
- 
- #  links = list(set(links))
- 
- print(len(precos))
- print(len(descricoes))
- print(len(lote_vendido))
- print(len(visitas))
- print(len(links))
- 
- df = []
- df.append(precos)
- df.append(descricoes)
- df.append(lote_vendido)
- df.append(visitas)
- df.append(links)
- df.append(imagens)
- df.append(bids)
- 
- 
- df_geral = pd.DataFrame(df).T
- df_geral.columns = ['preço', 'descrição','lote vendido', 'visitas', 'links', 'imagem', 'lances']
- #df_geral['Catalogo'] = leilao_catalogo
- dados = pd.concat([df_geral,dados])
- for coluna in dados.columns:
-   dados[coluna] = dados[coluna].str.strip()
- 
- preco_limpo = []
- for i in dados['preço']:
-   try:
-     limpo = float(i.split('R$ ')[1].replace(',',''))
-     preco_limpo.append(limpo)
-   except:
-     preco_limpo.append(-1)
- 
- dados['visitas'] = dados['visitas'].astype(int)
- dados['preço'] = preco_limpo
- dados['preço'] = dados['preço'].replace(r'^\s*$', np.nan, regex=True)
- dados['lote vendido'] = dados['lote vendido'].replace('None', np.nan, regex=True)
- dados = dados[dados['preço']>0].reset_index(drop=True)
-
- dados = dados.set_index('descrição')
- dados['valor_vendido'] = dados['lote vendido']*dados['preço']
- dados['lances'] = dados['lances'].astype(int)
- dados['lancado'] = dados['lances'].apply(lambda x: 1 if x > 0 else 0)
- dados['valor_vendido'] = dados['lancado']*dados['preço']
- dados['id'] = dados['links'].apply(lambda x: x.split('ID=')[1].split('&')[0])
- dados.sort_values([ 'valor_vendido' ], ascending=False, inplace=True)
-
- def busca_historico_lances(id_peca):
-  url = 'https://www.letravivaleiloes.com.br/ajax/le_historico_peca.asp'
-  data = {"id":id_peca}
-  r = urlopen(Request(url, data=urlencode(data).encode()))
-  html = r.read().decode('utf-8', 'ignore')
-  soup = BeautifulSoup(html, 'html.parser')
-
-  try:
-    data_ultima = soup.find('span').get_text()
-
-  except:
-    data_ultima = '-'
-    
-  try:
-    interessados = soup.get_text().split()[0].split('|')[1]
+ #criando as listas que serão os Datasets
+ @st.cache_data(ttl=600)
+ def load_data(numero):
+  # URL do endpoint da API
+  page=1
+  url = f"https://contaovos.dengue.mat.br/pt-br/api/lastcounting?key=ivtcarjsyxyfwyetfmfpdbmutmutbkhdsqumspoyriqrr&page={page}"
   
-  except:
-    interessados = 0
-    
-  lance_automatico = 'AUTOMATICO' in soup.get_text()
-
-
-  return lance_automatico, interessados, data_ultima
- 
- automatico = '-'
- interessados = '-'
- data_ultima = '-'
-
- #for i in dados['id']:
- #       print(i)
- #       valores = busca_historico_lances(i)
- #       automatico.append(valores[0])
- #       interessados.append(int(valores[1]))
- #       data_ultima.append(valores[2])
-
- dados['automatico'] = automatico
- dados['interessados'] = interessados
- dados['data_ultima'] = data_ultima
+  # Cria um DataFrame Pandas vazio para armazenar os dados
+  dados = pd.DataFrame()
   
-
+  
+  response = requests.get(url)
+  data = response.json()
+  df = pd.DataFrame(data)
+  dados = pd.concat([dados, df])
+  
+  # Loop até a resposta estar vazia
+  while len(df) != 0:
+  
+        # Incrementa o número da página no payload
+        page += 1
+        url = f"https://contaovos.dengue.mat.br/pt-br/api/lastcounting?key=ivtcarjsyxyfwyetfmfpdbmutmutbkhdsqumspoyriqrr&page={page}"
+  
+  
+        # Envia uma solicitação POST para o endpoint da API
+        response = requests.get(url)
+  
+        # Obtém a resposta JSON da API
+        data = response.json()
+  
+        # Converte a resposta JSON em um DataFrame Pandas
+        df = pd.DataFrame(data)
+  
+        # Concatena o novo DataFrame ao DataFrame existente
+        dados = pd.concat([dados, df])
+  
+        # Imprime o número da página atual
+        print(page)
+  
  return dados
+
 
 prg = st.progress(0) 
   
@@ -206,50 +69,58 @@ for i in range(100):
     time.sleep(0.1) 
     prg.progress(i+1) 
 
-dados = load_data('38762')
-dados['data_ultima'] = pd.to_datetime(dados['data_ultima'], errors='coerce', dayfirst=True)
-st.success("Banco de dados atualizado!")
+dados = load_data()
 
 
 
-col1, col2 = st.columns([1,4])
+#
+st.dataframe(dados)
 
-with col2:
-    st.dataframe(dados[['imagem','preço','lances','visitas','links']],
-                 use_container_width=True,
-                 height=600,
-                column_config={
-                'links':st.column_config.LinkColumn(
-                    'Link',
-                    width='medium'
-                ),
-                'imagem':st.column_config.ImageColumn(
-                  'Imagem',
-                  width='small'
-                ), 
-                'preço':st.column_config.NumberColumn(
-                  'Preço',
-                  format="R$%.2f",
-                  width='small'
-                ),
-                  'lances':st.column_config.NumberColumn(
-                  'Lances',
-                  width='small'
-                ),
-                })
+#Criar novo dataframe com os valores médios de cada ovitrampa
+dados_mapa_geral = pd.pivot_table(dados, index=['latitude','longitude', 'municipality', 'ovitrap_id'], values='eggs', aggfunc='mean').reset_index()
 
-with col1:
-  #st.metric('Potencial', f'R$ {dados["preço"].sum():,.2f}')
+#Criação do mapa
+#definição das cores
+dados_mapa_geral['cor'] = pd.cut(dados_mapa_geral['eggs'], bins=[-1,0,50,100,200,10000], labels=[ 'lightgray',#zero
+                                                                                                    'limegreen', # 1 a 50
+                                                                                                    'gold', #50 a 100
+                                                                                                    'orange', #100 a 200
+                                                                                                    'red' #mais de 200
+                                                                                                       ])
 
-  st.metric('Valor vendido', f'R$ {dados["valor_vendido"].sum():,.2f}')
+attr = ('Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+)
+tiles = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
 
-  st.metric('Valor Comissão', f'R$ {dados["valor_vendido"].sum()*0.05:,.2f}')
+m = folium.Map(location=[dados_mapa_geral.latitude.mean(), dados_mapa_geral.longitude.mean()],
+               zoom_start=5,
+               tiles=tiles, attr=attr
+               )
 
-  st.metric('Total de Visitas', dados['visitas'].sum())
+tooltip = "Click me!"
 
-  st.metric('Total de LAnces', dados['lances'].sum())
+folium.GeoJson('https://raw.githubusercontent.com/andrejarenkow/geodata/main/municipios_rs_CRS/RS_Municipios_2021.json',
+    style_function=lambda feature: {
+        "fillColor": "rgba(0,0,0,0)",
+        "color": "white",
+        "weight": 1,
+    },).add_to(m)
 
-  st.metric('Itens com lances', f"{(dados['lancado'].sum()/len(dados['lancado'])*100).round(1)} %")
+for linha in dados_mapa_geral.itertuples():
+  folium.Circle(
+      location=[linha.latitude, linha.longitude],
+      popup='Município %s - Armadilha %s - Ovos %s' % (linha.municipality, linha.ovitrap_id, linha.eggs),
+      radius=150,
+      color=linha.cor,
+      fill=True,
+      fill_color=linha.cor
+                   ).add_to(m)
+
+
+
+# call to render Folium map in Streamlit
+st_data = st_folium(m, width=725)
+
 
 
 
