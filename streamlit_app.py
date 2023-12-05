adf58238f1d2fa3bd07111cfec4b34957ebeaa8f
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2023-12-05 17:47:28 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -277,13 +277,13 @@ else:
 
  with col2:
   # call to render Folium map in Streamlit
-  tab1, tab2 = st.tabs(['Mapa de calor','Mapa com pontos'])
+  tab1, tab2 = st.tabs(['Mapa com pontos','Mapa de calor'])
   with tab1:
    st.write('Mapa de calor de todo estado do RS')
-   #map_plotly_fig_calor = px.density_mapbox(dados_mapa_todos, lat="latitude", lon="longitude", z="eggs", mapbox_style="satellite-streets",
-   #               color_continuous_scale='Reds', zoom=5, center=dict(lat=-30.456877333125696, lon= -53.01906610604057), height=600, radius=20)
-   map_plotly_fig_calor = ff.create_hexbin_mapbox(data_frame=dados_mapa_geral, lat='latitude', lon='longitude', nx_hexagon=100, opacity=1, color='eggs',
-                                                  mapbox_style="satellite-streets",color_continuous_scale='Reds',  labels={"color": "Número de ovos"})
+   map_plotly_fig_calor = px.scatter_mapbox(dados_mapa_todos, lat="latitude", lon="longitude", z="eggs", mapbox_style="satellite-streets",
+                  color_continuous_scale='Reds', zoom=5, center=dict(lat=-30.456877333125696, lon= -53.01906610604057), height=600, size_max=100)
+   #map_plotly_fig_calor = ff.create_hexbin_mapbox(data_frame=dados_mapa_geral, lat='latitude', lon='longitude', nx_hexagon=100, opacity=1, color='eggs',
+   #                                               mapbox_style="satellite-streets",color_continuous_scale='Reds',  labels={"color": "Número de ovos"})
    map_plotly_fig_calor.update_layout(template='plotly_dark', paper_bgcolor='rgba(0,0,0,0)',
                                 margin=go.layout.Margin(l=10, r=10, t=10, b=10),
                               mapbox_accesstoken= 'pk.eyJ1IjoiYW5kcmUtamFyZW5rb3ciLCJhIjoiY2xkdzZ2eDdxMDRmMzN1bnV6MnlpNnNweSJ9.4_9fi6bcTxgy5mGaTmE4Pw',
