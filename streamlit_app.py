11d424b603742f7b781b7d90e409632df81326ea
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2023-12-05 17:31:14 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -105,9 +105,13 @@ with filtros:
   municipio = st.selectbox('Selecione o município', options=lista_municipios, index=len(lista_municipios)-1)
  
  with col3:
-  mes = st.selectbox('Selecione o mês', options=sorted(dados[(dados['municipality']==municipio)&(dados['year']==ano)]['mes'].unique()))
-  semana_epidemiologica = dados[(dados['municipality']==municipio)&(dados['year']==ano)&(dados['mes']==mes)]['week'].values[0]
-  st.write(f'Semana epidemiológica {semana_epidemiologica}')
+  if municipio != 'Todos':
+   mes = st.selectbox('Selecione o mês', options=sorted(dados[(dados['municipality']==municipio)&(dados['year']==ano)]['mes'].unique()))
+   semana_epidemiologica = dados[(dados['municipality']==municipio)&(dados['year']==ano)&(dados['mes']==mes)]['week'].values[0]
+   st.write(f'Semana epidemiológica {semana_epidemiologica}')
+
+  else:
+   mes = st.selectbox('Selecione o mês', options=sorted(dados[(dados['year']==ano)]['mes'].unique()))
  
 
 
