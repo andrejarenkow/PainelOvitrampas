84728e0b3da04cc47a46adab7a24e8a980466467
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2023-12-05 14:52:20 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -241,10 +241,9 @@ if municipio != 'Todos':
 
   with tab2:
    st.subheader('Mapa de calor')
-   map_plotly_fig_calor = ff.create_hexbin_mapbox(data_frame=dados_mapa_geral, lat='latitude', lon='longitude', nx_hexagon=25, opacity=0.9, color='eggs',
+   map_plotly_fig_calor = ff.create_hexbin_mapbox(data_frame=dados_mapa_geral, lat='latitude', lon='longitude', nx_hexagon=30, opacity=0.9, color='eggs',
                                                   mapbox_style="satellite-streets",color_continuous_scale='Reds', show_original_data=True,
-                                                 original_data_marker=dict(size=4, opacity=0.8, color="black"), labels={"color": "Número de ovos"},
-                                                 min_count=1)
+                                                 original_data_marker=dict(size=4, opacity=0.8, color="black"), labels={"color": "Número de ovos"})
    map_plotly_fig_calor.update_layout(template='plotly_dark', paper_bgcolor='rgba(0,0,0,0)',
                                 margin=go.layout.Margin(l=10, r=10, t=10, b=10),
                               mapbox_accesstoken= 'pk.eyJ1IjoiYW5kcmUtamFyZW5rb3ciLCJhIjoiY2xkdzZ2eDdxMDRmMzN1bnV6MnlpNnNweSJ9.4_9fi6bcTxgy5mGaTmE4Pw',
