d66d504875241aa5a653b3609882befab3a4fd2e
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2023-12-05 17:48:30 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -280,7 +280,7 @@ else:
   tab1, tab2 = st.tabs(['Mapa com pontos','Mapa de calor'])
   with tab1:
    st.write('Mapa de calor de todo estado do RS')
-   map_plotly_fig_calor = px.scatter_mapbox(dados_mapa_todos, lat="latitude", lon="longitude", z="eggs", mapbox_style="satellite-streets",
+   map_plotly_fig_calor = px.scatter_mapbox(dados_mapa_todos, lat="latitude", lon="longitude", color="eggs", mapbox_style="satellite-streets",
                   color_continuous_scale='Reds', zoom=5, center=dict(lat=-30.456877333125696, lon= -53.01906610604057), height=600, size_max=100)
    #map_plotly_fig_calor = ff.create_hexbin_mapbox(data_frame=dados_mapa_geral, lat='latitude', lon='longitude', nx_hexagon=100, opacity=1, color='eggs',
    #                                               mapbox_style="satellite-streets",color_continuous_scale='Reds',  labels={"color": "Número de ovos"})
