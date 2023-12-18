a99971006f37c3c716e1c3f762fcef552f36b423
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2023-12-18 15:06:07 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -123,10 +123,10 @@ with aba_painel:
      #semana_epidemiologica = dados[(dados['municipality']==municipio)&(dados['year']==ano)&(dados['mes']==mes)]['week'].values[0]
      #st.write(f'Semana epidemiológica {semana_epidemiologica}')
   
-  #filtro_faixa_0_a_50 = 
-  #filtro_faixa_50_a_100 = 
-  #filtro_faixa_100_a_200 = 
-  #filtro_faixa_200_ou_mais =
+  filtro_faixa_0_a_50 = st.toggle('0 a 50')
+  filtro_faixa_50_a_100 = st.toggle('51 a 100')
+  filtro_faixa_100_a_200 = st.toggle('101 a 200')
+  filtro_faixa_200_ou_mais =st.toggle('Mais de 200')
   
  
  
